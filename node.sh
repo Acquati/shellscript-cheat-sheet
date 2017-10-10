7d1ce418cacdc2f8d3ef658ef8d04320a984c04b
acquati <leandroacquati@gmail.com>
2017-10-10 18:00:20 -0300

    alteracoes no  node.js

--- node.sh
+++ node.sh
@@ -2,17 +2,17 @@
 echo "Instalando o Node.js + NPM."
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+#export NVM_DIR="$HOME/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-nvm list
-nvm ls-remote
-nvm install 8.2.1
-nvm use 8.2.1
-nvm alias default 8.2.1
-node -v
-npm install -g npm
-npm -v
+#nvm list
+#nvm ls-remote
+#nvm install 8.2.1
+#nvm use 8.2.1
+#nvm alias default 8.2.1
+#node -v
+#npm install -g npm
+#npm -v
 
-echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+#echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
